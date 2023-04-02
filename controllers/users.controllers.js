b2cbd2c40370df3e0c8fc8488167a0e23e3e67e1
Arfanur Rahman <arfanur36@gmail.com>
2023-04-02 17:40:58 +0600

    3-6 Handle get and post request

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,15 +1,25 @@
-module.exports.getAllUsers = (req, res, next) => {
-	const { ip, query, params, body, headers } = req;
-	// console.log(ip, query, params, body, headers);
+const tools = [
+	{ id: 1, name: "Hammer" },
+	{ id: 2, name: "Hammer2" },
+	{ id: 3, name: "Hammer3" },
+];
 
-	// res.download(__dirname + "/users.controllers.js");
-	// res.json({ name: "abc" });
-	// res.redirect("/login");
-	res.send("found users");
+module.exports.getAllUsers = (req, res, next) => {
+	const { limit, page } = req.query;
+	console.log("limit, page :>> ", limit, page);
+	res.json(tools.slice(0, limit));
 };
 
-module.exports.saveAUser = (req, res) => {};
+module.exports.saveAUser = (req, res) => {
+	console.log(req.query);
+	tools.push(req.query);
+	res.send(tools);
+};
 
 module.exports.getUserDetail = (req, res) => {
-	res.send("user detail found");
+	const { id } = req.params;
+	console.log("🚀 ~ id:", id);
+	// const filter = {_id: id}
+	const foundTool = tools.find((tool) => tool.id === Number(id));
+	res.send(foundTool);
 };
--- middleware/limiter.js
+++ middleware/limiter.js
@@ -3,7 +3,7 @@ const { rateLimit } = require("express-rate-limit");
 /* The bellow code is limiting the number of requests that can be made to the server. */
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
